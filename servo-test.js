0d527c4af90e0473abaa4f72fe31f3dd5ca5ea62
Martin Gontovnikas <martin@gon.to>
2015-06-14 15:36:48 -0700

    Finished servo API

--- servo-test.js
+++ servo-test.js
@@ -1,4 +1,16 @@
 var servo = require('./servo');
 
-servo(90);
-servo(90, false);
+console.log("Initing");
+servo.init()
+.then(function() {
+  console.log("About to move 90deg");
+  return servo.move(90, true);
+}, function(err) {
+  console.log("Error", err);
+})
+.then(function() {
+  console.log("About to move 90deg back");
+  return servo.move(90, false);
+}, function(err) {
+  console.log("Errors", err);
+});
--- servo.js
+++ servo.js
@@ -1,7 +1,7 @@
 var tessel = require('tessel');
 var servolib = require('servo-pca9685');
 var Constants = require('./constants');
-var Promise = require('promise');
+var Promise = require('q').Promise;
 
 // Config
 var servo = servolib.use(tessel.port[Constants.SERVO_PORT]);
@@ -10,11 +10,14 @@ var servoNumber = Constants.SERVO_NUMBER;
 
 var loaded = new Promise(function(resolve, reject) {
   servo.on('ready', function () {
+    console.log("Ready");
     servo.configure(servoNumber, 0.05, 0.14, function () {
-      resolve();  
+      console.log("Configured");
+      resolve(true);  
     });
   });
   servo.on('error', function (err) {
+    console.log("there was an error");
     reject(err);
   });
 });
@@ -26,6 +29,7 @@ var read = function() {
         if (err) {
           return reject(err);
         }
+        console.log("Current position", reading);
         return resolve(reading);
       })
     });
@@ -38,12 +42,19 @@ function move(degrees, forward) {
     if (!forward) {
       movement = reading - movement;
     }
+    if (movement < 0) {
+      movement = 0;
+    }
+    if (movement > 1) {
+      movement = 1;
+    }
+    console.log("About to move", movement, degrees, forward);
     return new Promise(function(resolve, reject) {
       servo.move(servoNumber, movement, function(err) {
         if (err) {
           return reject(err);
         }
-        return resolve();
+        return resolve(true);
       });
     });
   });  
@@ -52,12 +63,13 @@ function move(degrees, forward) {
 function init() {
   return loaded.then(function() {
     return new Promise(function(resolve, reject) {
-      servo.move(servoNumber, movement, function(err) {
+      servo.move(servoNumber, 0, function(err) {
         if (err) {
           return reject(err);
         }
-        return resolve();
-      })
+        console.log("Moved to start position");
+        return resolve(true);
+      });
     });
     
   })
