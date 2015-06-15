14da3c23cc2ac99c5ca6069be33c85018e979465
Sandrino Di Mattia <contact@sandrino.be>
2015-06-15 11:08:25 -0500

    Bluetooth sample

---
+++ bluetooth.js
@@ -0,0 +1,61 @@
+var tessel = require('tessel');
+var adv = require('bleadvertise');
+
+var blePort = tessel.port['B'];
+ble = require('ble-ble113a').use(blePort, function(err) {
+
+	var service = adv.serialize({
+		completeName: "HyperLock"
+	});
+
+	console.log('Service:', service);
+
+	ble.on('startAdvertising', function() {
+		console.log('Start advertising.');
+	});
+
+	ble.on('stopAdvertising', function() {
+		console.log('Stop advertising.');
+	});
+
+	ble.on('connect', function(connection) {
+		console.log('Connect:', connection);
+	});
+
+	ble.on('disconnect', function(connection) {
+		console.log('Disconnect:', connection);
+	});
+
+	ble.on('remoteWrite', function(connection, index, valueWritten) {
+		if (index === 5) {
+			console.log('Remote Write:', valueWritten.toString('ascii'));
+		}
+	});
+
+	ble.on('remoteReadRequest', function(connection, index) {
+		console.log('Remote Read Request:', connection, index);
+	});
+
+	ble.on('remoteNotification', function(connection, index) {
+		console.log('Remote Notification:', connection, index);
+	});
+
+	ble.on('remoteIndication', function(connection) {
+		console.log('Remote Indication:', connection);
+	});
+
+	ble.on('remoteUpdateStop', function(connection) {
+		console.log('Remote Update Stop:', connection);
+	});
+
+	ble.on('indicated', function(connection) {
+		console.log('Indicated:', connection);
+	});
+
+	ble.setAdvertisingData(service, function(err) {
+		if (err) {
+			return console.log(err);
+		}
+		ble.startAdvertising();
+	});
+});
