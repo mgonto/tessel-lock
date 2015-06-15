01f95e373b556123364b996b41231f8380bf39ed
Cristian Douce <cristian@gravityonmars.com>
2015-06-15 13:01:31 -0500

    Fixes rebase

--- index.js
+++ index.js
@@ -6,30 +6,23 @@ var hyperlock = require('auth0-hyperlock-client');
 
 var servoInit = servo.init(config.servo);
 
-function lock() {
-  return servoInit.then(function() {
-    return servo.move(120, true);
-  });
-}
-
-function unlock() {
-  return servoInit.then(function() {
-    return servo.move(120, false);
-  });
-} 
 bluetooth.init();
+
 bluetooth.on("wifi-configure", function(data){
   config.NETWORK = data;
 });
+
 bluetooth.on("wifi-release", function(data){
   // wifi.disconnect();
 });
+
 bluetooth.on("wifi-connect", function(data){
-  wifi.connect(config.NETWORK).then(networkready);
+  wifi.connect(config.NETWORK).then(networkready, networkerror);
 });
+
 bluetooth.on("hyperlock-pair", function(data){
-  
-}); 
+
+});
 
 function networkready(data) {
   var client = hyperlock.create_lock_client({
@@ -56,11 +49,11 @@ function networkready(data) {
 
   client.on('error', function(err) {
     console.log("Hyperlock crashed", err);
-    process.exit(0);
+    process.exit(1);
   })
 }
 
 function networkerror(err) {
   console.log("Cannot connect to Wifi", err);
-  process.exit(0);
-};
+  process.exit(1);
+}
